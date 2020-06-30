b452e86b00a83799a5c77acbfaa9d3b170fc309d
Roberto Gama <robertofgama@gmail.com>
2020-06-30 10:15:51 -0300

    Reorganização da area de arrays e variaveis

--- darth-instaulous.sh
+++ darth-instaulous.sh
@@ -5,12 +5,10 @@
 create_variables src/repositorios.yaml
 
 # Funções
+. src/variaveis.sh
 . src/funcoes.sh
 . src/menus.sh
 
-# Aplicativos para Instalar
-repositorios=($telegram_apt_repositorio_ppa $logitech_mouse_driver_apt_repositorio_ppa)
-aplicativos=($anki_apt_app $telegram_apt_app $nodejs_apt_app $neovim_apt_app $powerline_apt_app $tilix_apt_app $tmux_apt_app $terminal_programas_apt_app $logitech_mouse_driver_apt_app $mpv_apt_app) 
 
 case $1 in
   -i) instalarAplicativos ;;
---
+++ src/variaveis.sh
@@ -0,0 +1,17 @@
+# Aplicativos para Instalar
+repositorios=(
+$telegram_apt_repositorio_ppa
+$logitech_mouse_driver_apt_repositorio_ppa
+)
+
+aplicativos=(
+$anki_apt_app
+$telegram_apt_app
+$nodejs_apt_app
+$neovim_apt_app
+$powerline_apt_app
+$tilix_apt_app
+$tmux_apt_app
+$terminal_programas_apt_app
+$logitech_mouse_driver_apt_app
+$mpv_apt_app)
