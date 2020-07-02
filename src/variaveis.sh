b4cb72817a175fc6a91f0c3ae9507d9031167974
Roberto Gama <robertofgama@gmail.com>
2020-07-02 15:32:40 -0300

    Adição do OBS Studio com repositório oficial

--- src/variaveis.sh
+++ src/variaveis.sh
@@ -1,8 +1,9 @@
 # Aplicativos para Instalar
 repositorios=(
 $java_oracle_apt_repositorio_ppa
-$telegram_apt_repositorio_ppa
 $logitech_mouse_driver_apt_repositorio_ppa
+$obsstudio_apt_repositorio_ppa
+$telegram_apt_repositorio_ppa
 )
 
 aplicativos=(
@@ -12,6 +13,7 @@ $logitech_mouse_driver_apt_app
 $mpv_apt_app
 $nodejs_apt_app
 $neovim_apt_app
+$obsstudio_apt_app
 $powerline_apt_app
 $telegram_apt_app
 $terminal_programas_apt_app
