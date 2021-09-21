2ce1d6986e785573d7cede43176ab0998bf2564c
Roberto Gama <robertofgama@gmail.com>
2021-09-21 11:12:34 -0300

    Alias para autoupdate

--- confs/shells/aliases.sh
+++ confs/shells/aliases.sh
@@ -1,3 +1,4 @@
+alias di-up='.darth-instaulous/./darth-instaulous.sh -up'
 alias l='ls -lFh'
 alias @gitf='git add . && git commit -m "Atualização" && git push origin master'
 alias @loopback='pactl load-module module-loopback'
