dba42426324719b15a2d9ab64fd48579acc20c66
Roberto Gama <robertofgama@gmail.com>
2021-09-21 11:34:00 -0300

    Correcao de Alias

--- confs/shells/aliases.sh
+++ confs/shells/aliases.sh
@@ -1,4 +1,4 @@
-alias di-up='.darth-instaulous/./darth-instaulous.sh -up'
+alias di-up='$HOME/.darth-instaulous/./darth-instaulous.sh -up'
 alias l='ls -lFh'
 alias @gitf='git add . && git commit -m "Atualização" && git push origin master'
 alias @loopback='pactl load-module module-loopback'
